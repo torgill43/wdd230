0ff766fdac67299579c3dcd5660cedc860175303
torgill43 <timorgill43@gmail.com>
2022-10-09 00:00:41 -0600

    main page

---
+++ chamber/js/main.js
@@ -0,0 +1,20 @@
+const date_now = new Date();
+console.log(date_now);
+
+const full_date = new Intl.DateTimeFormat ('en-US', {dateStyle: 'full'}).format(date_now);
+console.log(full_date);
+document.querySelector('.current-date').textContent = full_date;
+
+let year = date_now.getFullYear();
+document.querySelector('#year').textContent = year;
+
+let currentDate = document.lastModified;
+document.querySelector('#updated').textContent = currentDate;
+
+function toggleMenu() {
+    document.querySelector('#primaryNav').classList.toggle("open");
+}
+
+const x = document.querySelector('#hamburger-button');
+
+x.addEventListener('click', toggleMenu);
