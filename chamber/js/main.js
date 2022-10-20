4b278248efac1b1925a933460c6a8a961abbf32a
torgill <org18001@byui.edu>
2022-10-19 22:12:55 -0600

    news and weather

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -35,3 +35,17 @@ function toggleMessage() {
 if (weekDay == 1 || weekDay == 2) {
     toggleMessage()
 }
+
+// Wind Chill
+let temp = document.querySelector('#temp').textContent;
+let windspeed = document.querySelector('#wind-speed').textContent;
+
+if (temp < 50 && windspeed > 4.8) {
+let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
+
+const windchill = document.querySelector('#wind-chill');
+
+windchill.innerHTML = chill + '&#8457;';
+
+}
+
